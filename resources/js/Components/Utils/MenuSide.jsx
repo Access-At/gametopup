b5d806c9c0ea22639dd3057ac8a68cc05cb1a02a
farhanreizha <reizha226@gmail.com>
2022-10-02 18:49:06 +0700

    Fix: sidebar route

--- resources/js/Components/Utils/MenuSide.jsx
+++ resources/js/Components/Utils/MenuSide.jsx
@@ -16,23 +16,23 @@ export default function MenuSide({ setIsOpen }) {
         <FaTimes className="text-white h-5 w-5" />
       </button>
       <div className="flex flex-col gap-3 mr-3 sticky top-20">
-        <Link href={route('home')} className={url === '/' ? "navbarMenuBottomActive" : "navbarMenuBottomNonActive"}>
+        <Link href={route('home')} className={`${url === '/' ? "bg-primary-500 p-2 md:bg-secondry-500 md:text-primary-500" : ""} flex items-center gap-2`}>
           <FaHome className="font2xl" />
           <span className="font-semibold text-lg">Home</span>
         </Link>
-        <Link href={route('price')} className={url === '/price' ? "navbarMenuBottomActive" : "navbarMenuBottomNonActive"}>
+        <Link href={route('price')} className={`${url === '/price' ? "bg-primary-500 p-2 md:bg-secondry-500 md:text-primary-500" : ""} flex items-center gap-2`}>
           <MdOutlinePriceChange className="font2xl" />
           <span className="font-semibold text-lg">Price</span>
         </Link>
-        <Link href={route('invoice')} className={url === '/check-invoice' ? "navbarMenuBottomActive" : "navbarMenuBottomNonActive"}>
+        <Link href={route('invoice')} className={`${url === '/check-invoice' ? "bg-primary-500 p-2 md:bg-secondry-500 md:text-primary-500" : ""} flex items-center gap-2`}>
           <FaSearchDollar className="font2xl" />
           <span className="font-semibold text-lg">Check Order</span>
         </Link>
-        <Link href={route('about')} className={url === '/about-store' ? "navbarMenuBottomActive" : "navbarMenuBottomNonActive"}>
+        <Link href={route('about')} className={`${url === '/about-store' ? "bg-primary-500 p-2 md:bg-secondry-500 md:text-primary-500" : ""} flex items-center gap-2`}>
           <BsCreditCard2Front className="font2xl" />
           <span className="font-semibold text-lg">About Store</span>
         </Link>
-        <Link className={url === '/other' ? "navbarMenuBottomActive" : "navbarMenuBottomNonActive"}>
+        <Link className={`${url === '/other' ? "bg-primary-500 p-2 md:bg-secondry-500 md:text-primary-500" : ""} flex items-center gap-2`}>
           <TbQuestionMark className="font2xl" />
           <span className="font-semibold text-lg">Other</span>
         </Link>
