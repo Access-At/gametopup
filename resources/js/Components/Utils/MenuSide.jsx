3b0574968e530018da5b2cb788b4fff2d372f901
farhanreizha <reizha226@gmail.com>
2022-10-02 18:36:35 +0700

    Fix: Sidebar Route

--- resources/js/Components/Utils/MenuSide.jsx
+++ resources/js/Components/Utils/MenuSide.jsx
@@ -5,36 +5,36 @@ import { TbQuestionMark } from "react-icons/tb";
 import { Link } from "@inertiajs/inertia-react";
 
 export default function MenuSide({ setIsOpen }) {
-    return (
-        <>
-            <button
-                className="absolute flex justify-end px-5 py-7 right-6"
-                onClick={() => setIsOpen(false)}
-            >
-                <FaTimes className="text-white h-5 w-5" />
-            </button>
-            <div className="flex flex-col gap-3 mr-3 sticky top-20">
-                <Link className="flex items-center gap-2">
-                    <FaHome className="w-6 h-6" />
-                    <span className="font-semibold text-lg">Home</span>
-                </Link>
-                <Link className="flex items-center gap-2">
-                    <MdOutlinePriceChange className="w-6 h-6" />
-                    <span className="font-semibold text-lg">Price</span>
-                </Link>
-                <Link className="flex items-center gap-2">
-                    <FaSearchDollar className="w-6 h-6" />
-                    <span className="font-semibold text-lg">Check Order</span>
-                </Link>
-                <Link className="flex items-center gap-2">
-                    <BsCreditCard2Front className="w-6 h-6" />
-                    <span className="font-semibold text-lg">About Store</span>
-                </Link>
-                <Link className="flex items-center gap-2">
-                    <TbQuestionMark className="w-6 h-6" />
-                    <span className="font-semibold text-lg">Other</span>
-                </Link>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <button
+        className="absolute flex justify-end px-5 py-7 right-6"
+        onClick={() => setIsOpen(false)}
+      >
+        <FaTimes className="text-white h-5 w-5" />
+      </button>
+      <div className="flex flex-col gap-3 mr-3 sticky top-20">
+        <Link href={route('home')} className={url === '/' ? "navbarMenuBottomActive" : "navbarMenuBottomNonActive"}>
+          <FaHome className="font2xl" />
+          <span className="font-semibold text-lg">Home</span>
+        </Link>
+        <Link href={route('price')} className={url === '/price' ? "navbarMenuBottomActive" : "navbarMenuBottomNonActive"}>
+          <MdOutlinePriceChange className="font2xl" />
+          <span className="font-semibold text-lg">Price</span>
+        </Link>
+        <Link href={route('invoice')} className={url === '/check-invoice' ? "navbarMenuBottomActive" : "navbarMenuBottomNonActive"}>
+          <FaSearchDollar className="font2xl" />
+          <span className="font-semibold text-lg">Check Order</span>
+        </Link>
+        <Link href={route('about')} className={url === '/about-store' ? "navbarMenuBottomActive" : "navbarMenuBottomNonActive"}>
+          <BsCreditCard2Front className="font2xl" />
+          <span className="font-semibold text-lg">About Store</span>
+        </Link>
+        <Link className={url === '/other' ? "navbarMenuBottomActive" : "navbarMenuBottomNonActive"}>
+          <TbQuestionMark className="font2xl" />
+          <span className="font-semibold text-lg">Other</span>
+        </Link>
+      </div>
+    </>
+  );
 }
