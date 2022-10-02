6801f088e82d2510c1e1f295e2507e3e4519359e
bayupb <kampuspb.bayu@gmail.com>
2022-10-02 16:05:07 +0700

    change name

--- resources/js/Components/PagesComponent/NavbarBottom.jsx
+++ resources/js/Components/PagesComponent/NavbarBottom.jsx
@@ -7,23 +7,23 @@ export default function NavbarBottom() {
     return (
         <div className="bg-secondry-600 sticky bottom-0 p-2 block lg:hidden">
             <div className="flex justify-between text-2xl md:mx-12 mx-4">
-                <div className="navbar_menu_bottom_active">
+                <div className="navbarMenuBottomActive">
                     <FaHome className="font2xl" />
                     Home
                 </div>
-                <div className="navbar_menu_bottom_non_active">
+                <div className="navbarMenuBottomNonActive">
                     <MdOutlinePriceChange className="font2xl" />
                     Price
                 </div>
-                <div className="navbar_menu_bottom_non_active">
+                <div className="navbarMenuBottomNonActive">
                     <FaSearchDollar className="font2xl" />
                     Check Order
                 </div>
-                <div className="navbar_menu_bottom_non_active">
+                <div className="navbarMenuBottomNonActive">
                     <BsCreditCard2Front className="font2xl" />
                     About Store
                 </div>
-                <div className="navbar_menu_bottom_non_active">
+                <div className="navbarMenuBottomNonActive">
                     <TbQuestionMark className="font2xl" />
                     Other
                 </div>
